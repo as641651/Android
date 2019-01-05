29066e88b46da4c40b1c062b03f9b1b92cc8582d
Aravind Sankaran <aravind.sankaran@rwth-aachen.de>
2019-01-05 22:24:02 +0530

    Added java refresher

---
+++ JavaPackagesAndCompiling_files/4760317_002.js
@@ -0,0 +1,4 @@
+(function(id,src){if(document.getElementById(id)){return;}
+var js=document.createElement('script');js.src=src;js.type='text/javascript';js.id=id;var e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(js,e);})('hs-analytics','//js.hs-analytics.net/analytics/1546697700000/4760317.js');(function(id,src,attrs){if(document.getElementById(id)){return;}
+var js=document.createElement('script');js.src=src;js.type='text/javascript';js.id=id;for(var name in attrs){if(attrs.hasOwnProperty(name)){js.setAttribute(name,attrs[name]);}}
+var e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(js,e);})('LeadFlows-4760317','https://js.hsleadflows.net/leadflows.js',{"crossorigin":"anonymous","data-leadin-portal-id":4760317,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":4760317,"data-hsjs-env":"prod"});
